f43ba6e0e0043c1779ff1384336133a6658dd06e
Timothy Wesley <timothy.wesley99@yahoo.com>
2024-11-25 01:04:40 -0500

    added eye detection to make it more precise and redue false positives

--- Facial Recognition.py
+++ Facial Recognition.py
@@ -7,8 +7,7 @@ chat_id = "7250352955"
 
 cap = cv2.VideoCapture(1)
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
-
+eye_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{token_id}/sendMessage"
     params = {"chat_id": chat_id, "text": message}
@@ -20,10 +19,15 @@ def send_telegram_message(message):
 
 def face_detection(video):
     gray_image = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
-    face = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+    face = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=8, minSize=(40, 40))
     for (x, y, w, h) in face:
-        cv2.rectangle(video, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        return True
+        roi_gray = gray_image[y:y+h, x:x+w]
+        eyes = eye_classifier.detectMultiScale(roi_gray)
+
+        if len(eyes) % 2 == 0 :
+            return True
+        else:
+            return False
     else:
         return False
 
